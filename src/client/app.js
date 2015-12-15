ceb79945115023a7ad2f220800a15a50cb3d4ce7
Vu Nam <vunam281@gmail.com>
2015-12-15 11:38:19 +0000

    Picking up and making it run

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TubeList from './componentes/tubelist';
+import TubeList from './components/tubelist';
 
 ReactDOM.render(
   <TubeList url="https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw" />
